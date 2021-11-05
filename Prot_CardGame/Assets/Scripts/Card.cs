9b289575b04bb94e1ba7a1a6dc872f69b4675baa
AnDreY-MA <andreymamontov9@gmail.com>
2021-11-05 11:19:47 +0300

    Commit
    Реализовано поднятие карты при наведении

--- Prot_CardGame/Assets/Scripts/Card.cs
+++ Prot_CardGame/Assets/Scripts/Card.cs
@@ -15,19 +15,27 @@ public class Card : MonoBehaviour
         posCard = transform.position;
     }
 
+    public void OnMouseEnter()
+    {
+        transform.position = new Vector3(transform.position.x - 0.5f, transform.position.y + 1, transform.position.z);
+    }
+
+    private void OnMouseExit()
+    {
+        transform.position = posCard;
+    }
+
     private void OnMouseOver()
     {     
             if (Input.GetMouseButtonDown(0))
             {
                 if (countClick == 0)
                 {
-                    transform.position = new Vector3(transform.position.x - 0.5f, transform.position.y + 1, transform.position.z);
                     countClick += 1;
                     placeCard.GetComponent<SpriteRenderer>().color = Color.red;
                 }
                 else
                 {
-                    transform.position = posCard;
                     countClick = 0;
                     placeCard.GetComponent<SpriteRenderer>().color = Color.white;
                 }
